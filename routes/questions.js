ff3edae16defdc6c92995a5e38185f47efd1bb25
themythicaldrago <ryan.louie@students.olin.edu>
2014-11-15 03:15:44 -0500

    Set up dummy data and function for adding questions

---
+++ routes/questions.js
@@ -0,0 +1,81 @@
+var mongo = require('mongodb');
+ 
+var Server = mongo.Server,
+    Db = mongo.Db,
+    BSON = mongo.BSONPure;
+ 
+var server = new Server('localhost', 27017, {auto_reconnect: true});
+db = new Db('boolsdb', server);
+ 
+db.open(function(err, db) {
+    if(!err) {
+        console.log("Connected to 'boolsdb' database");
+        db.collection('questions', {strict:true}, function(err, collection) {
+            if (err) {
+                console.log("The 'questions' collection doesn't exist. Creating it with sample data...");
+                populateDB();
+            }
+        });
+    }
+});
+
+/*
+RESTFUL API HEREEE
+*/
+exports.findAll = function(req, res) {
+    db.collection('questions', function(err, collection) {
+        collection.find().toArray(function(err, items) {
+            res.send(items);
+        });
+    });
+};
+
+exports.findById = function(req, res) {
+    var id = req.params.id;
+    console.log('Retrieving question: ' + id);
+    db.collection('questions', function(err, collection) {
+        collection.findOne({'_id':new BSON.ObjectID(id)}, function(err, item) {
+            res.send(item);
+        });
+    });
+};
+
+exports.addQuestion = function(req, res) {
+    var question = req.body;
+    console.log('Adding question: ' + JSON.stringify(question));
+    db.collection('questions', function(err, collection) {
+        collection.insert(question, {safe:true}, function(err, result) {
+            if (err) {
+                res.send({'error':'An error has occurred'});
+            } else {
+                console.log('Success: ' + JSON.stringify(result[0]));
+                res.send(result[0]);
+            }
+        });
+    });
+}
+
+/*--------------------------------------------------------------------------------------------------------------------*/
+// Populate database with sample data -- Only used once: the first time the application is started.
+// You'd typically not find this code in a real-life app, since the database would already exist.
+var populateDB = function() {
+ 
+    var questions = [
+    {
+        question: "0 or 1",
+        optionA: "0",
+        optionB: "1",
+        answers: {"user1":"0"}
+    },
+    {
+        question: "Apples or Bananas",
+        optionA: "apple",
+        optionB: "banana",
+        answers: {"user1":"apple"}
+    }];
+ 
+    db.collection('questions', function(err, collection) {
+        collection.insert(questions, {safe:true}, function(err, result) {});
+    });
+ 
+};
--- server.js
+++ server.js
@@ -1,10 +1,20 @@
-var express = require('express');
+var express = require('express'),
+	question = require('./routes/questions');
  
 var app = express();
+
+app.configure(function () {
+    app.use(express.logger('dev'));     /* 'default', 'short', 'tiny', 'dev' */
+    app.use(express.bodyParser());
+});
 
 app.get('/', function (req, res) {
   res.send('Hello Bool!')
 });
- 
+
+app.get('/questions', question.findAll);
+app.get('/questions/:id', question.findById);
+app.post('/questions', question.addQuestion);
+
 app.listen(3000);
 console.log('Listening on port 3000...');
