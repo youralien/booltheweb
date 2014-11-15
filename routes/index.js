9d773191ba79e467b91330c5bda5178f083b84f4
Sawyer Vaughan <sawyer.vaughan@students.olin.edu>
2014-11-15 11:42:21 -0500

    restructured the database. automatically adds poster field

--- routes/index.js
+++ routes/index.js
@@ -50,6 +50,7 @@ module.exports = function(passport){
 		res.redirect('/');
 	});
 
+	//router.get('/deleteall', question.deleteAll);
 
 	/* GET Ask [Questions]*/
 	router.get('/ask', function(req, res) {
@@ -59,7 +60,7 @@ module.exports = function(passport){
 
 	router.get('/questions', question.findAll);
 	router.get('/questions/:id', question.findById);
-	router.put('/questions/:id', question.addVote);
+	router.put('/questions/:id', question.addAnswer);
 	router.post('/questions', question.addQuestion);
 	router.delete('/questions/:id', question.deleteQuestion);
 
