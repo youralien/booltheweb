30ed76073674a3ec7c845d64f0206bd00beac9a7
Sawyer Vaughan <sawyer.vaughan@students.olin.edu>
2014-11-16 07:49:26 -0500

    why is mine different

--- public/javascripts/me.js
+++ public/javascripts/me.js
@@ -109,19 +109,7 @@ $(document).ready(function() {
 	        series: [{
 	            type: 'pie',
 	            name: 'Browser share',
-	            data: [
-	                ['Firefox',   45.0],
-	                ['IE',       26.8],
-	                {
-	                    name: 'Chrome',
-	                    y: 12.8,
-	                    sliced: true,
-	                    selected: true
-	                },
-	                ['Safari',    8.5],
-	                ['Opera',     6.2],
-	                ['Others',   0.7]
-	            ]
+	            data: l
 	        }]
 	    });
 	}
